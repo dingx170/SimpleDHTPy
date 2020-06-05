04981e4613e2cc1a26f33a2ee01ff85a0473b384
Tong Ding <dingtong@seattleu.edu>
2020-06-05 13:57:33 -0700

    working code

--- dht_client.py
+++ dht_client.py
@@ -1,41 +1,72 @@
+"""
+A DHT client that manages key-value pairs through DHT
+:Author: Tong (Debby) Ding
+:See CPSC 5510, Seattle University
+"""
 import sys
 import socket
 import pickle
 import hashlib 
 
-class Client:
-    def __init__(self, action, key, value):
-        self.key = key
-        self.value = value
-        self.action = self._get_action(action)
-        # self.key_hash = self._hash_key()
-        self.key_hash = int(key[-1]) # del
+class DHTClient:
+    """ A class used to represent a DHT client"""
+    
+    def __init__(self, method, key, value):
+        self._key = key
+        self._value = value
+        self._action = self._get_action(method)
+        self._key_hash = self._hash_key()
 
     def send_request(self, node, node_port):
+        """Send request to DHT with given node information
+
+        :param node(str): The hostname of the node
+        :param node_port(str): The port number of the node
+        """
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             server_addr = (node, node_port)
             s.sendto(self._create_message(), server_addr)
-            data, server = s.recvfrom(1024)
-            msg_dict = pickle.loads(data)
-            print("-------\nClient received msg:\n-------\n{}".format(msg_dict))
+
+            # set a timeout for getting response
+            s.settimeout(3)
+            try:
+                data, server = s.recvfrom(1024)
+                response = pickle.loads(data)
+            except socket.timeout:
+                print("Timeout for connection to DHT")
+                exit(1)
+
+            print("-------\nClient received msg:\n-------\n{}"
+                  .format(response))
     
-    def _get_action(self, action):
-        if action == "get" and self.value == "":
+    def _get_action(self, method):
+        """Identify the action on data requested by client
+
+        :param method: GET/PUT method indicated by client
+        :type method: str
+        :rtype: str
+        """
+        if method == "get" and self._value == "":
             return "read"
-        elif action == "put" and self.value == "":
+        elif method == "put" and self._value == "":
             return "delete"
-        elif action == "put" and self.value != "":
+        elif method == "put" and self._value != "":
             return "write"
-        else:
-            print("invalid action") 
+
+        print("Invalid action") 
+        exit(1)
 
     def _create_message(self):
-        msg_dict = {"action" : self.action, "key_hash" : self.key_hash, "key" : self.key, "value" : self.value, "hops" : 0}
-        return pickle.dumps(msg_dict)
+        """Create the request message to DHT"""
+        request = {"action" : self._action, 
+                   "key_hash" : self._key_hash, 
+                   "key" : self._key, 
+                   "value" : self._value}
+        return pickle.dumps(request)
     
     def _hash_key(self):
-        key_hash = hashlib.sha1(self.key.encode())
-        key_hex = key_hash.hexdigest()
+        _key_hash = hashlib.sha1(str.encode(self._key))
+        key_hex = _key_hash.hexdigest()
         key_int = int(key_hex, 16)
         return key_int
 
@@ -51,7 +82,7 @@ if __name__ == '__main__':
         print("Usage: python3 dht_client.py node nodePort get|put key [value]")
         exit(1)
     
-    client = Client(action, key, value)
-    # client.print_key_id()
-    client.send_request(node, int(node_port))
+    client = DHTClient(action.lower(), key.lower(), value.lower())
+    client.send_request(node.lower(), int(node_port))
+    exit(0)
     
--- dht_node.py
+++ dht_node.py
@@ -5,17 +5,23 @@ import hashlib
 
 class DHTNode:
     def __init__(self, host_file, line_num):
-        self.nodes = self._get_nodes(host_file)
-        self.host = self.nodes[line_num][0]
-        self.port = int(self.nodes[line_num][1])
-        # self.node_id = self._get_node_id()
-        self.node_id = int(self.nodes[line_num][1][-2]) # del
-        self.storage = {"Oregon4" : 1234} # fix-me
-        self._successor = self._find_successor()
-        self._predecessor = self._find_predecessor()
+        self._nodes = self._get_nodes(host_file)
+        self._hashed_nodes = self._hash_nodes()
+        self.host = self._nodes[line_num][0]
+        self.port = int(self._nodes[line_num][1])
+        self.node_id = self._get_node_id(self.host, self.port)
+        # self.node_id = int(self._nodes[line_num][1][-2]) # del
+        self._succ = self._find_succ()
+        self._pred = self._find_pred()
         self._finger_table = self._prep_table()
+        self.storage = {}
     
     def _get_nodes(self, host_file):
+        """Get a list of DHT nodes name and IP number from a file
+
+        :param host_file(str): the file containing node information
+        :rtype: list of tuples
+        """ 
         with open(host_file) as f:
             nodes = []
             for line in f:
@@ -23,113 +29,99 @@ class DHTNode:
                 ip = socket.gethostbyname(name)
                 nodes.append((ip, port))
         return nodes
+    
+    # fix-me
+    def _hash_nodes(self):
+        """Get a dictionary of the nodes' hash value and address"""
+        nodes = {}
+        for node in self._nodes:
+            ip, port = node
+            node_id = self._get_node_id(ip, port)
+            # node_id = int(str(port)[-2]) # del
+            nodes[node_id] = node
+        return nodes
 
     def run(self):
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
             s.bind((self.host, self.port))
+
             while True:
                 data, address = s.recvfrom(1024)
                 request = pickle.loads(data)
-                print("---------------------\nDHT node {} rcvd msg:\n---------------------".format(self.node_id))
-                print("action : {}".format(request["action"]))
-                print("keyhash: {}".format(request["key_hash"]))
-                print("key    : {}".format(request["key"]))
-                print("value  : {}".format(request["value"]))
-                print("hops   : {}".format(request["hops"]))
-                print("---------------------\nmy storage:\n---------------------")
-                for key, val in self.storage.items():
-                    print("{} : {}".format(key, val))
-                print("---------------------")
 
+                # check if this node is the first node the client contacts
+                if "hops" not in request:
+                    request["hops"] = 0
+                    request["client"] = address
+
+                self._print_rcvd_msg(request)
+
+                # find the proper node to forward the request to
+                node_id = self._find_fwd_node(request)
                 response = {}
-                if request["key_hash"] == self._predecessor:
-                    print("----> key goes to predecessor")
-                    break
-                elif self._predecessor < request["key_hash"] <= self.node_id:
-                    print("----> key goes to me")
-                    response = self._process_request(request) 
+
+                # reply to client if this node maches the hashed key 
+                if node_id == self.node_id:
+                    response = self._process_request(request)
+                    s.sendto(pickle.dumps(response), request["client"])
                     print(response)
 
-                elif self.node_id < request["key_hash"] <= self._successor:
-                    print("----> key goes to successor")
-                    break
+                # otherwise, forward request to the proper node
                 else:
-                    print("----> check finger table <----")
-                    finger_table = self._finger_table
+                    ip, port = self._hashed_nodes[node_id]
+                    request["hops"] = request["hops"] + 1
+                    s.sendto(pickle.dumps(request), (ip, int(port)))
 
-                    node_entries = list(finger_table.keys())                      
-                    if request["key_hash"] >= node_entries[-1] or request["key_hash"] < self._predecessor:
-                        print("--> key goes to {} : {}".format(node_entries[-1], finger_table[node_entries[-1]]))
-                    else: 
-                        for i in range(len(node_entries) - 1, 0, -1):
-                            print("compare range: {} - {}".format(node_entries[i-1], node_entries[i]))
-                            if node_entries[i-1] <= request["key_hash"] < node_entries[i]:
-                                print("--> key goes to {} : {}".format(node_entries[i-1], finger_table[node_entries[i-1]]))
-                                break
-                print("\n")
-
-                s.sendto(pickle.dumps(response), address)
-                # show_storage()
-    
     def _process_request(self, request):
+        action = request["action"]
         request["status"] = "OK"
-        if request["action"] == "read":
+        request["hops"] = request["hops"] + 1
+        
+        if action == "read":
             try:
                 request["value"] = self.storage[request["key"]]
             except KeyError:
                 request["status"] = "error: key not found"
-  
-        elif request["action"] == "write":
+
+        elif action == "write":
             self.storage[request["key"]] = request["value"]
             
-        elif request["action"] == "delete":
+        elif action == "delete":
             try:
                 self.storage.pop(request["key"])
             except KeyError:
                 request["status"] = "error: key not found"
 
-
-        request["hops"] = request["hops"] + 1
-
         return request
 
-    # def _form_response(self, action, key_hash)
-
-    def _find_successor(self):
-        nodes = self._hash_nodes()
-        node_ids = list(nodes.keys())
+    def _find_succ(self):
+        """Get the successor of a node"""
+        node_ids = list(self._hashed_nodes.keys())
         node_ids.sort()
 
         if node_ids[-1] == self.node_id:
             return node_ids[0]
         
         pos = node_ids.index(self.node_id)
-
-        print("\n------------------")
-        print("* successor  : {}".format(node_ids[pos + 1]))
         return node_ids[pos + 1]
 
-    def _find_predecessor(self):
-        nodes = self._hash_nodes()
-        node_ids = list(nodes.keys())
+    def _find_pred(self):
+        """Get the predecessor of a node"""
+        node_ids = list(self._hashed_nodes.keys())
         node_ids.sort()
 
         if node_ids[0] == self.node_id:
             return node_ids[-1]
         
         pos = node_ids.index(self.node_id)
-        
-        print("* predecessor: {}".format(node_ids[pos - 1]))
-        print("------------------")
         return node_ids[pos - 1]
-
 
     # fix-me
     def _prep_table(self):
-        m = 4
+        m = 160 # del 4
         node_entries = [(self.node_id + 2**(k-1))%(2**m) for k in range(1, m+1)]
-        nodes = self._hash_nodes()
-        node_ids = list(nodes.keys())
+
+        node_ids = list(self._hashed_nodes.keys())
 
         node_entries.sort()
         node_ids.sort()
@@ -152,41 +144,76 @@ class DHTNode:
             print("   {} : {}".format(key, val))
         return finger_table
 
-
-
-
-
-    # fix-me
-    def _hash_nodes(self):
-        nodes = {}
-        for node in self.nodes:
-            ip, port = node
-            node_id = int(str(port)[-2])
-            nodes[node_id] = node
-        return nodes
-
-
-    # def _check_id(self, ):
-              
-    def _get_node_id(self):
-        data = self.host + str(self.port)
-        node_id_hash = hashlib.sha1(data.encode())
+    def _get_node_id(self, host, port):
+        host_b = socket.inet_pton(socket.AF_INET, host)
+        port_b = int(port).to_bytes(2, byteorder = "big")
+        node_id_hash = hashlib.sha1(host_b + port_b)
         node_id_hex = node_id_hash.hexdigest()
         node_id_int = int(node_id_hex, 16)
         return node_id_int
-
-    # def show_storage(self):
-    #     print(self.storage)
-
-    def print_nodes(self):
-        for node in self.nodes:
-            print(node)
     
-    # def print_me(self):
-    #     print("{}, {}".format(self.host, self.port))
-    
-    def print_id(self):
-        print("node_id : {}".format(self.node_id))
+    def _print_rcvd_msg(self, request):
+        print("---------------------\nDHT node {} information:\n---------------------".format(self.node_id))
+        print("* predecessor: {}".format(self._pred))
+        print("* my_node  : {}".format(self.node_id))
+        print("* successor  : {}".format(self._succ))
+        print("---------------------\nDHT node {} rcvd msg:\n---------------------".format(self.node_id))
+        print("action : {}".format(request["action"]))
+        print("keyhash: {}".format(request["key_hash"]))
+        print("key    : {}".format(request["key"]))
+        print("value  : {}".format(request["value"]))
+        print("hops   : {}".format(request["hops"]))
+        print("---------------------\nDHT node {} storage:\n---------------------".format(self.node_id))
+        for key, val in self.storage.items():
+            print("{} : {}".format(key, val))
+        print("---------------------")
+        print("PRINT --- PRED --- KEY --- NODE_ID --- SUCC")
+        print(self._pred, request["key_hash"], self.node_id, self._succ)
+        print()
+        print()
+        print()
+
+    def _find_fwd_node(self, request):
+        key = request["key_hash"]
+
+        if key == self._pred:
+            print("----> key goes to predecessor")
+            return self._pred
+            
+        if self._pred > self.node_id:
+            if self._pred < key or key <= self.node_id:
+                print("----> key goes to me")
+                return self.node_id
+        else: 
+            if self._pred < key <= self.node_id:
+                print("----> key goes to me")
+                return self.node_id
+
+        if self._succ < self.node_id:
+            if self.node_id < key or key <= self._succ:
+                print("----> key goes to successor")
+                return self._succ
+        else:
+            if self.node_id < key <= self._succ:
+                print("----> key goes to successor")
+                return self._succ
+            
+        if key > self._succ:
+            print("----> check finger table <----")
+            table = self._finger_table
+            nodes = list(table.keys())         
+                         
+            if key >= nodes[-1]:
+                return table[nodes[-1]]
+            else: 
+                for i in range(len(nodes) - 1, 0, -1):
+                    print("compare range: {} - {}".format(nodes[i-1], nodes[i]))
+                    if nodes[i-1] <= key < nodes[i]:
+                        print("--> key goes to {} : {}".format(nodes[i-1], table[nodes[i-1]]))
+                        return table[nodes[i-1]]
+        
+        print("----> key goes to predecessor<----")
+        return self._pred
 
 if __name__ == '__main__':
 
@@ -203,10 +230,7 @@ if __name__ == '__main__':
         print('{} is not a digit'.format(line_num))
         exit(1)
 
-    node = DHTNode(host_file, line_num)
-    # node.print_id()
-    # node.print_nodes()
-    
+    node = DHTNode(host_file.lower(), line_num)
     node.run()
     
 
